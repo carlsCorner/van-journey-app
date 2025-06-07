54c1da3394621d4e74f6f720269a2fc68ea6fd33
Matthew Carlin <matthew.michael.carlin@gmail.com>
2025-06-06 19:06:50 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// This file is included for potential future interactivity.
+// For the current setup with multi-page navigation and direct map links,
+// no JavaScript is strictly necessary for basic functionality.
